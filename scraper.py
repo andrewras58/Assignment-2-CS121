0d29cfd1f7339599d4d0fd44a45ac2c52f7ff2d4
andrewras58 <andrewras58@gmail.com>
2022-04-16 22:43:15 -0700

    aras - cleared out binary search stuff

--- scraper.py
+++ scraper.py
@@ -151,32 +151,3 @@ def is_valid(url):
             + r"img|sql)$", parsed.path.lower()):
         return False
     return True
-
-# Binary search to find the 2 most similar hashes in a sorted list
-# Code edited from: https://www.tutorialspoint.com/python-program-to-implement-binary-search-without-recursion
-def search_hashes(hashes: [int], query: int) -> (int, int):
-    low = 0
-    high = len(hashes) - 1
-    while low <= high:
-        mid = (high + low) // 2
-        if hashes[mid] < query:
-            low = mid + 1
-        elif hashes[mid] > query:
-            high = mid - 1
-    return hashes[low], hashes[high]
-
-# checks to see how similar the number is and returns True or False based on a given threshold
-def hash_similarity(query: int, sim_pair: (int, int), threshold: float) -> bool:
-    return 1-abs(sim_pair[0]-query)/query > threshold or 1-abs(sim_pair[1]-query)/query > threshold
-
-# A modified version of selection sort that only puts the newest element where it needs to be
-# Code edited from: https://www.geeksforgeeks.org/python-program-for-insertion-sort/
-def insert_to_hashes(hashes: [int], new_hash: int) -> None:
-    hashes.append(new_hash)
-    i = len(hashes) - 1
-    j = i - 1
-    key = hashes[i]
-    while j >= 0 and key < hashes[j]:
-        hashes[j+1] = hashes[j]
-        j -= 1
-    hashes[j+1] = key
