fb10e30ead3cb32bd14295b2e4034461680e6c3e
Epsil-m <anemail@address.com>
2022-04-17 12:04:37 -0700

    Fix simhashing, should be more accurate

--- scraper.py
+++ scraper.py
@@ -11,7 +11,27 @@ nltk.download('punkt')
 
 Blacklist = set()
 Visited = set()
-Stop_Words = {"a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "aren't", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "can't", "cannot", "could", "couldn't", "did", "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during", "each", "few", "for", "from", "further", "had", "hadn't", "has", "hasn't", "have", "haven't", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its", "itself", "let's", "me", "more", "most", "mustn't", "my", "myself", "no", "nor", "not", "of", "off", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "shan't", "she", "she'd", "she'll", "she's", "should", "shouldn't", "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "wasn't", "we", "we'd", "we'll", "we're", "we've", "were", "weren't", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "won't", "would", "wouldn't", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves"}
+Stop_Words = {"a", "about", "above", "after", "again", "against", "all", "am",
+    "an", "and", "any", "are", "aren't", "as", "at", "be", "because", "been", 
+    "before", "being", "below", "between", "both", "but", "by", "can't", 
+    "cannot", "could", "couldn't", "did", "didn't", "do", "does", "doesn't", 
+    "doing", "don't", "down", "during", "each", "few", "for", "from", 
+    "further", "had", "hadn't", "has", "hasn't", "have", "haven't", "having", 
+    "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", 
+    "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", 
+    "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its", "itself",
+    "let's", "me", "more", "most", "mustn't", "my", "myself", "no", "nor", 
+    "not", "of", "off", "on", "once", "only", "or", "other", "ought", "our", 
+    "ours", "ourselves", "out", "over", "own", "same", "shan't", "she", 
+    "she'd", "she'll", "she's", "should", "shouldn't", "so", "some", "such", 
+    "than", "that", "that's", "the", "their", "theirs", "them", "themselves", 
+    "then", "there", "there's", "these", "they", "they'd", "they'll", 
+    "they're", "they've", "this", "those", "through", "to", "too", "under", 
+    "until", "up", "very", "was", "wasn't", "we", "we'd", "we'll", "we're", 
+    "we've", "were", "weren't", "what", "what's", "when", "when's", "where", 
+    "where's", "which", "while", "who", "who's", "whom", "why", "why's", 
+    "with", "won't", "would", "wouldn't", "you", "you'd", "you'll", "you're", 
+    "you've", "your", "yours", "yourself", "yourselves"}
 Longest_Page = ('Default', 0)
 Common_Words = defaultdict(int)
 Subdomain = defaultdict(int) # {key = Subdomains under ics.uci.edu domain, value = a counter of unique Pages}
@@ -117,7 +137,7 @@ def extract_next_links(url, resp):
 
         # Stop duplicates of same link by splitting
         # (ex #ref40, #ref45 etc of same link)
-        # not sure if including '?' is necessary, neef further testing
+        # not sure if including '?' is necessary, need further testing
         href = href.split('#')[0]
         href = href.split('?')[0]
 
@@ -190,13 +210,13 @@ def create_simhash(resp):
     sortFreq = OrderedDict(sorted(freq.items(), key=lambda kv: kv[1], reverse=True))
 
     # get set of keys to convert to hex and store in list
-    wordSet = list(sortFreq.keys())
+    wordList = list(sortFreq.keys())
 
     hashList = list()
     binList = list()
 
     # Convert each word to hex and store
-    for word in wordSet:
+    for word in wordList:
         hashList.append(hashlib.sha1(word.encode()).hexdigest())
 
     # Convert each hex into 160 bit binary string and store
@@ -208,9 +228,10 @@ def create_simhash(resp):
 
     for pos in range(160):
         # Get the weight to calculate
-        weight = sortFreq[wordSet[pos]]
+        
         num = 0
         for binNum in binList:
+            weight = sortFreq[wordList[pos]]
             binary = binNum[pos]
             num += weight if binary == '1' else -weight
     
